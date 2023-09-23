d0dbbd2402cb7f5ab0d8766542e2d691c94cfea3
Matheus Colombo <111023158+mathcolombo@users.noreply.github.com>
2023-09-23 16:40:51 -0300

    nada dms

--- princ.java
+++ princ.java
@@ -59,18 +59,16 @@ public class princ {
         return 0;
     }
 
-    public static ArrayList<ArrayList<Object>>InfoVoos() { // posição : 0 = número do avião / 1 = origem / 2 = destino / 3 horário de partida / 4 = horário de chegada / 5 = quantidade máx de passageiros
+    //Subprograma 
+    public static ArrayList<ArrayList<Object>> InfoVoos() { // posição : 0 = número do avião / 1 = origem / 2 = destino / 3 horário de partida / 4 = horário de chegada / 5 = quantidade máx de passageiros
 
-         ArrayList<Object> av1 = new ArrayList<>(Arrays.asList(2904, "São Paulo", "Espírito Santo", "13:00", "17:00", 100));
+        ArrayList<Object> av1 = new ArrayList<>(Arrays.asList(2904, "São Paulo", "Espírito Santo", "13:00", "17:00", 100));
+        ArrayList<Object> av2 = new ArrayList<>(Arrays.asList(3650, "Madri", "Cariacica", "13:00", "17:00", 100));
 
-         ArrayList<Object> av2 = new ArrayList<>(Arrays.asList(3650, "Madri", "Cariacica", "13:00", "17:00", 100));
-
-         ArrayList<ArrayList<Object>> voos = new ArrayList<>();
-         voos.add(av1);
-         voos.add(av2);
-
+        ArrayList<ArrayList<Object>> voos = new ArrayList<>();
+        voos.add(av1);
+        voos.add(av2);
 
         return voos;
-
     }
 }
