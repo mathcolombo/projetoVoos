9b687f0470e8fa03926e639b552a1b2e31fe0aad
Marcelo Meneguci <marcelo.reis@souunisales.com.br>
2023-11-29 17:56:29 -0300

    atualizacao dos menus
    atualizacao dos menus de usuario e volta dos dados necessarios para realizacao da solicitacao de reserva do voo

--- MenuUsuario.java
+++ MenuUsuario.java
@@ -1,15 +1,20 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MenuUsuarioComum {
+public class MenuUsuario {
 
     private static Scanner scan = new Scanner(System.in);
     public static Voos voos = new Voos();
     
 
-    public void exibirMenu() {
+    public void exibirMenuComum() {
         int opcao;
         do {
+
+            
+
+            
+
             System.out.println("-------------Menu-------------");
             System.out.println("1 - Exibir Informações de Voo");
             System.out.println("2 - Realizar reserva");
@@ -29,10 +34,7 @@ public class MenuUsuarioComum {
                     break;
                 case 2: // Reserva
                     
-                    System.out.println("Voos disponíveis: ");
-
-
-                    /*System.out.println("Para realizar uma reserva é necessário realizar o cadastro!!");
+                    System.out.println("Primeiro Realize o cadastro"); // informacoes dos passageiros
                     System.out.println("Inserir informações do usuário");
                     System.out.println("================================================");
                     System.out.println("Digite o nome do passageiro:");
@@ -47,10 +49,13 @@ public class MenuUsuarioComum {
                     System.out.println("Digite o email do passageiro:");
                     String email = scan.next();
                     System.out.println("================================================");
-                    Passageiro passageiro = new Passageiro(nome, idade, cpf, email);*/
+                    Passageiro passageiro = new Passageiro(nome, idade, cpf, email);
+
+                    System.out.println("");
+                    System.out.println("Voos disponíveis: ");
+                    System.out.println("");
 
                     voos.ReservarVoo();
-
 
                     break;
                 case 3: //Check-in
@@ -61,6 +66,41 @@ public class MenuUsuarioComum {
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (opcao != 0);
+    }
+    
+     public void exibirMenuAdmin() {
+        int opcao;
+        do {
+
+            
+
+            
+
+            System.out.println("-------------Menu-------------");
+            System.out.println("1 - Exibir Informações de Voo");
+            System.out.println("2 - Autorizar Rezerva");
+            System.out.println("3 - Realizar checking");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = scan.nextInt();
+
+            switch (opcao) {
+                case 1: //Informações dos voos
+
+                    voos.infoVoos();
+                    System.out.println();
+                    System.out.println("---------------------------------------------------");
+
+                    break;
+                case 2: // Reservas Pendentes 
+
+
+                    break;
+                case 3: //Check-in
+
             }
         } while (opcao != 0);
     }
